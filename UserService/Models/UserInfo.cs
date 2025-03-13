8a9b24cc55363f0edecd8b67de28d86eeabd8663
StylianosNikopoulos <nikstel2001@gmail.com>
2025-03-13 21:07:45 +0200

    UserService models
    +fix versions

---
+++ UserService/Models/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UserService.Models;
+
+public partial class UserInfo
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string ResumePath { get; set; } = null!;
+
+    public DateTime? CreatedAt { get; set; }
+}
---
+++ UserService/Models/UserServiceDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserService.Models;
+
+public partial class UserServiceDbContext : DbContext
+{
+    public UserServiceDbContext()
+    {
+    }
+
+    public UserServiceDbContext(DbContextOptions<UserServiceDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<UserInfo> UserInfos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=127.0.0.1;database=UserServiceDB;user=ResumeAnalyzer;password=prst1234", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.3.0-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<UserInfo>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("UserInfo");
+
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.ResumePath).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
