b561b0aea7e0f4c4d6ce3ce9fb8839a29a0d6f2a
StylianosNikopoulos <nikstel2001@gmail.com>
2025-03-14 20:50:29 +0200

    UserService
    Resumes

--- UserService/Controllers/UserController.cs
+++ UserService/Controllers/UserController.cs
@@ -3,14 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UserService.Models;
 
 namespace UserService.Controllers
 {
+    [Route("api/user")]
+    [ApiController]
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly UserServiceDbContext _context;
+
+        public UserController(UserServiceDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+
+        [HttpPost("upload-resume")]
+        public async Task<IActionResult> UploadResume([FromForm] IFormFile file, [FromForm] int userId)
+        {
+            var files = Request.Form.Files;
+
+            if (files.Count == 0)
+                return BadRequest("No files received.");
+
+            var resume = files[0]; 
+
+            if (resume == null || resume.Length == 0)
+            {
+                return BadRequest("Invalid file");
+            }
+
+            var filePath = Path.Combine("Resumes", $"{Guid.NewGuid()}_{resume.FileName}");
+            Directory.CreateDirectory("Resumes"); 
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await resume.CopyToAsync(stream);
+            }
+
+            var user = await _context.UserInfos.FindAsync(userId);
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            user.ResumePath = filePath;
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+
+        [HttpGet("resumes")]
+        public IActionResult GetResumes()
+        {
+            var resumes = _context.UserInfos   //Select
+                .ToList();
+
+            return Ok(resumes);
+        }
+
+        [HttpGet("download-resume/{userId}")]
+        public IActionResult DownloadResume(int UserId)
+        {
+            var user = _context.UserInfos.Find(UserId);
+            if(user == null || string.IsNullOrEmpty(user.ResumePath))
+            {
+                return NotFound("Resume not found");
+            }
+
+            var filePath = user.ResumePath;
+            var fileBytes = System.IO.File.ReadAllBytes(filePath);
+
+            return File(fileBytes, "application/pdf", Path.GetFileName(filePath));
         }
     }
 }
--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -7,7 +10,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<UserServiceDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
