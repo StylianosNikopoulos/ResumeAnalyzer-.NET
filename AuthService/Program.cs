e8c8b448b6a668b987e4e95385147022750d3d99
StylianosNikopoulos <nikstel2001@gmail.com>
2025-03-15 17:57:44 +0200

    MVC
    add MVC

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -3,17 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
 
 var connectionString = builder.Configuration.GetConnectionString("AuthServiceConnection");
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AuthServiceDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
---
+++ ResumeAnalyzerMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ResumeAnalyzerMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
