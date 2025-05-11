72e6436e292a56b9a461c40b1e1f25fc75bc9e78
StylianosNikopoulos <nikstel2001@gmail.com>
2025-05-11 14:32:00 +0300

    UI progress

--- ResumeAnalyzerMVC/Controllers/ResumesController.cs
+++ ResumeAnalyzerMVC/Controllers/ResumesController.cs
@@ -19,6 +19,8 @@ namespace ResumeAnalyzerMVC.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
+            ViewBag.auth = !string.IsNullOrEmpty(HttpContext.Session.GetString("UserToken"));
+
             var (success, resumesOrMessage, statusCode) = await _resumesHandler.ShowResumesAsync();
 
             if (!success)
